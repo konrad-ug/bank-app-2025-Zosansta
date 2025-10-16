9813cf0f192d7b588bb781ba86d9ffaec1051627
Zosia <zosansta@wp.pl>
2025-10-16 15:34:45 +0200

    zmiany 2

--- src/account.py
+++ src/account.py
@@ -1,5 +1,9 @@
 class Account:
-    def __init__(self, first_name, last_name):
+    def __init__(self, first_name, last_name, pesel):
         self.first_name = first_name
         self.last_name = last_name
         self.balance = 0.0
+        self.pesel = pesel
+        self.pesel = pesel if self.is_pesel_valid(pesel) else "Invalid"
+
+    # def is_pesel_valid(self, pesel):
--- tests/unit/test_account_create.py
+++ tests/unit/test_account_create.py
@@ -3,7 +3,20 @@ from src.account import Account
 
 class TestAccount:
     def test_account_creation(self):
-        account = Account("John", "Doe")
+        account = Account("John", "Doe", "12457897334")
         assert account.first_name == "John"
         assert account.last_name == "Doe"
-        assert account.balance == 00
+        assert account.balance == 0.0
+        assert account.pesel == "12457897334"
+
+    def test_pesel_too_long(self):
+        account = Account("John", "Doe", "12457897334")
+        assert account.pesel == "Invalid"
+    
+    def test_pesel_too_short(self):
+        account = Account("John", "Doe", "127334")
+        assert account.pesel == "Invalid"
+
+    def test_pesel_too_long(self):
+        account = Account("John", "Doe", None)
+        assert account.pesel == "Invalid"
