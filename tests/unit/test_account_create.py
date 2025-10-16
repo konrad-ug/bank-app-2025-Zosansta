60c89e1146682654be953e5340746d43da1b499d
Zosia <zosansta@wp.pl>
2025-10-16 16:01:00 +0200

    zmiany3

--- tests/unit/test_account_create.py
+++ tests/unit/test_account_create.py
@@ -20,3 +20,19 @@ class TestAccount:
     def test_pesel_too_long(self):
         account = Account("John", "Doe", None)
         assert account.pesel == "Invalid"
+
+    def test_correct_promo_code(self):
+        account = Account("John", "Doe", "12457897334", "PROM_123")
+        assert account.balance == 50.0
+
+    def test_correct_promo_code_too_long(self):
+        account = Account("John", "Doe", "12457897334", "PROM_1552")
+        assert account.balance == 0.0
+
+    def test_correct_promo_code_too_short(self):
+        account = Account("John", "Doe", "12457897334", "PROM_52")
+        assert account.balance == 0.0
+
+    def test_correct_promo_code_wrong_prefix(self):
+        account = Account("John", "Doe", "12457897334", "PROM-52")
+        assert account.balance == 0.0
