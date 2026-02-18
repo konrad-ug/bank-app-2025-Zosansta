11a69c777b65c4b23c3f5c3c57a597b83f944d70
Zosia <zosansta@wp.pl>
2026-02-18 12:49:25 +0100

    Finalna refaktoryzacja testów

--- tests/unit/test_company_transwer.py
+++ tests/unit/test_company_transwer.py
@@ -1,21 +1,22 @@
+import pytest
 from src.company_account import CompanyAccount
 
-
 class TestCompanyAccount:
+
+
     def test_company_account_creation(self):
-            company_account = CompanyAccount("Nazwa_firmy", "1234678915")
-            assert company_account.name == "Nazwa_firmy"
-            assert company_account.balance == 0.0
-            assert company_account.nip == "1234678915"
+        company_account = CompanyAccount("Nazwa_firmy", "1234678915")
+        assert company_account.name == "Nazwa_firmy"
+        assert company_account.balance == 0.0
+        assert company_account.nip == "1234678915"
 
-    def test_nip_too_long(self):
-        company_account = CompanyAccount("Nazwa_firmy", "1245789347334")
-        assert company_account.nip == "Invalid"
-    
-    def test_pesel_too_short(self):
-        company_account = CompanyAccount("Nazwa_firmy", "124534")
-        assert company_account.nip == "Invalid"
 
-    def test_pesel_none(self):
-        company_account = CompanyAccount("Nazwa_firmy", None)
-        assert company_account.nip == "Invalid"
+    @pytest.mark.parametrize("nip, expected_nip", [
+        ("1234567890", "1234567890"),
+        ("1234567890123", "Invalid"),
+        ("123456", "Invalid"),
+        (None, "Invalid")
+    ])
+    def test_nip_validation(self, nip, expected_nip):
+        company_account = CompanyAccount("Nazwa_firmy", nip)
+        assert company_account.nip == expected_nip
